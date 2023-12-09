27712f95d70b00812cdbe3ac33cbb7f9740f85f1
Renish G <renishghetia@gmail.com>
2023-12-09 11:31:47 +0530

    react native project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+function App(): React.JSX.Element {
+  return <></>;
+}
+
+export default App;
